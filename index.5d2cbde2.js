950024f4f41ec07401fb6aa2778e043309ab042f
bihoi-giulia <118809730+bihoi-giulia@users.noreply.github.com>
2023-06-09 06:31:31 +0000

    Deploying to gh-pages from @ bihoi-giulia/goit-js-hw-11@0a38ec23cb0bae97a2cbc119a156a2b40852b110 🚀

---
+++ index.5d2cbde2.js
@@ -0,0 +1,2 @@
+let e=0;const t=document.querySelector(".scroll"),l=()=>{0===window.pageYOffset&&clearInterval(e),window.scroll(0,window.pageYOffset-50)};t.addEventListener("click",(()=>{e=setInterval(l,16.66)}));
+//# sourceMappingURL=index.5d2cbde2.js.map
---
+++ index.5e351d66.js
@@ -0,0 +1,2 @@
+!function(){var e=0,n=document.querySelector(".scroll"),t=function(){0===window.pageYOffset&&clearInterval(e),window.scroll(0,window.pageYOffset-50)};n.addEventListener("click",(function(){e=setInterval(t,16.66)}))}();
+//# sourceMappingURL=index.5e351d66.js.map
